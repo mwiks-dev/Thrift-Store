cf3fdb57e69aa844530de7269e633522c6697336
Maryann Mwikali <mwikali119@gmail.com>
2023-01-03 17:18:53 +0300

    use before action to trigger a filter during authorization

--- app/controllers/cloths_controller.rb
+++ app/controllers/cloths_controller.rb
@@ -1,11 +1,18 @@
 class ClothsController < ApplicationController
+    before_action :authorize
+
     def index
         render json: Cloth.all
     end
 
     def show
-        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
         cloth = Cloth.find_by(id: params[:id])
         render json: cloth
+    end
+
+    private
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     end
 end
