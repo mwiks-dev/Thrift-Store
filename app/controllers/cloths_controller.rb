0b4810de916038bc35be386307c2bb9a1ec06124
Maryann Mwikali <mwikali119@gmail.com>
2023-01-03 17:14:58 +0300

    configure authorizations

--- app/controllers/cloths_controller.rb
+++ app/controllers/cloths_controller.rb
@@ -2,4 +2,10 @@ class ClothsController < ApplicationController
     def index
         render json: Cloth.all
     end
+
+    def show
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+        cloth = Cloth.find_by(id: params[:id])
+        render json: cloth
+    end
 end
